76afe0bfebdec60b377f330db2fc4631ec3a2b24
Shravan30032013 <167631894+Shravan30032013@users.noreply.github.com>
2024-05-18 21:11:52 +0530

    Add files via upload

---
+++ Project 3.py
@@ -0,0 +1,8 @@
+#Password Guessing
+
+Password="Shravan_coding_superstar576"
+Password_checker=input("Type the password to enter: ")
+while Password_checker!=Password:
+    print("Wrong password")
+    Password_checker=input("Type the password to enter: ")
+print("Correct password, you may enter")
